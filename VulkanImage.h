cff5f6c242f283fbf916c019b90033b8ad7a82d6
Peter Ferranti <onthegrid007@outlook.com>
2022-04-13 02:11:03 -0700

    Licensing

--- VulkanImage.h
+++ VulkanImage.h
@@ -1,3 +1,10 @@
+/*
+*   BSD 3-Clause License, see file labled 'LICENSE' for the full License.
+*   Copyright (c) 2022, Peter Ferranti
+*   All rights reserved.
+*   Other Contributers:
+*/
+
 #ifndef VULKANIMAGE_H_
 #define VULKANIMAGE_H_
 namespace VulkanUtilities {
--- platformdetection.h
+++ platformdetection.h
@@ -1,3 +1,10 @@
+/*
+*   BSD 3-Clause License, see file labled 'LICENSE' for the full License.
+*   Copyright (c) 2022, Peter Ferranti
+*   All rights reserved.
+*   Other Contributers:
+*/
+
 #ifdef _WIN32
 	#ifdef _WIN64
 		#define VULKAN_PLATFORM_WINDOWS
